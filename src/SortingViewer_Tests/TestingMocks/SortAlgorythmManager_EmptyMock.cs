7673b268f38c6ceda48cdf9f0705c1f8ff10e9d9
MrMame <51000524+MrMame@users.noreply.github.com>
2021-11-09 21:52:16 +0100

    Added to Mocks : SortAlgorythmManager_EmptyMock

---
+++ src/SortingViewer_Tests/TestingMocks/SortAlgorythmManager_EmptyMock.cs
@@ -0,0 +1,22 @@
+﻿using SortingViewer.Controller;
+using SortingViewer.Model.SortAlgorythm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingViewer_Tests.TestingMocks {
+    class SortAlgorythmManager_EmptyMock : ISortAlgorythmManager {
+        public void AddAlgorythm(string UniqueName, ISortAlgorythm SortAlgorythm) {
+        }
+
+        public ISortAlgorythm GetAlgorythm(string SortAlgorythmName) {
+            return null;
+        }
+
+        public List<string> GetSortAlgorythmsNames() {
+            return null;
+        }
+    }
+}
