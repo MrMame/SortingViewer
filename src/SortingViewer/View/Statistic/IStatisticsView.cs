f4d3e46ee8374354a643ecc51b13cdda6269c3c0
MrMame <51000524+MrMame@users.noreply.github.com>
2021-11-09 21:20:59 +0100

    Added IStatisticsView and StatisticsViewDebugMock

--- src/SortingViewer/View/Statistic/IStatisticsView.cs
+++ src/SortingViewer/View/Statistic/IStatisticsView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SortingViewer.Model.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace SortingViewer.View.Statistic
 {
     public interface IStatisticsView
     {
+        void ShowStatistics(IStatisticValues Stats);
+        void ResetView();
     }
 }
--- src/SortingViewer_Tests/TestingMocks/StatisticsViewDebugPrintMock.cs
+++ src/SortingViewer_Tests/TestingMocks/StatisticsViewDebugPrintMock.cs
@@ -1,4 +1,6 @@
-﻿using SortingViewer.View.Statistic;
+﻿using NUnit.Framework;
+using SortingViewer.Model.Data;
+using SortingViewer.View.Statistic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +9,12 @@ using System.Threading.Tasks;
 
 namespace SortingViewer_Tests.TestingMocks {
     class StatisticsViewDebugPrintMock : IStatisticsView {
-        
+        public void ResetView() {
+            TestContext.WriteLine("IStatisticsView.ResetView() has Reset the Statistics.");
+        }
+
+        public void ShowStatistics(IStatisticValues Stats) {
+            TestContext.WriteLine(Stats.ToString());
+        }
     }
 }
