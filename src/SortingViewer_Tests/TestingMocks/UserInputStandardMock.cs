a3a6e1d049ec2448792278c2ae1647b6ccb80f5e
MrMame <51000524+MrMame@users.noreply.github.com>
2021-11-09 21:46:12 +0100

    Created UserInputStandardMock to TestMocks

---
+++ src/SortingViewer_Tests/TestingMocks/UserInputStandardMock.cs
@@ -0,0 +1,34 @@
+﻿using SortingViewer.Model.Data;
+using SortingViewer.View.UserInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingViewer_Tests.TestingMocks {
+    class UserInputStandardMock : IUserInput {
+        public event EventHandler StartSort;
+        public event EventHandler StopSort;
+        public event EventHandler<SetSortValuesEventArgs> SetValues;
+        public event EventHandler<SetSortAlgorythmEventArgs> SetSortAlgorythm;
+
+
+        public void LoadSortAlgorythmNames(string[] SortAlgoNames) {
+        }
+
+        public void Do_StartSort() {
+            if(StartSort != null) StartSort(this, null);
+        }
+        public void Do_StopSort() {
+            if(StopSort != null) StopSort(this, null);
+        }
+        public void Do_SetValues(ISortValues Values) {
+            if(SetValues != null) SetValues(this, new SetSortValuesEventArgs() { SortValues = Values});
+        }
+        public void Do_SetSortAlgorythm(string SortAlgoName) {
+            if(SetSortAlgorythm != null) SetSortAlgorythm(this, new SetSortAlgorythmEventArgs() { SortAlgorythmName = SortAlgoName });
+        }
+
+    }
+}
