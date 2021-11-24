68e03ede3e7c3f0cfea29d299af3ffac9bf90e54
MrMame <51000524+MrMame@users.noreply.github.com>
2021-11-24 19:55:09 +0100

    Changed Returntype of factoryMethod

--- src/SortingViewer/View/Value/ValueViewFactory.cs
+++ src/SortingViewer/View/Value/ValueViewFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SortingViewer.View.Value {
     public class ValueViewFactory {
-        public static IValueView CreateValueBarView() {
+        public static ValuesBarView CreateValueBarView() {
             ValuesBarView vv = new ValuesBarView();
             vv.Dock = System.Windows.Forms.DockStyle.Fill;
             return vv;
