9dc9883ef7041bfcbe24f260837cc2432489bda4
MrMame <51000524+MrMame@users.noreply.github.com>
2021-11-25 17:49:06 +0100

    Added Endless Sort Algorythm Mock

---
+++ src/SortingViewer_Tests/TestingMocks/SortAlgorythm_EndlessMock.cs
@@ -0,0 +1,31 @@
+﻿using SortingViewer.Model.Data.SortValues;
+using SortingViewer.Model.SortAlgorythm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SortingViewer_Tests.TestingMocks {
+    /// <summary>
+    /// Implements an endless running sort algorythm that generates a value cahnged event each 100ms
+    /// </summary>
+    class SortAlgorythm_EndlessMock : ISortAlgorythm {
+        public event EventHandler<ValueChangedEventArgs> ValueChanged;
+        public event EventHandler<SortFinishEventArgs> SortFinish;
+
+        public void DoSort(ISortValues Values) {
+            int nShifts = 0;
+            int nSteps = 0;
+
+            while(true) {
+                // only value changed events are generated.
+                Thread.Sleep(100);
+                nShifts++;
+                if(ValueChanged != null) ValueChanged(this, new ValueChangedEventArgs() { StepNumber = nSteps, NumberShifts = nShifts });
+
+            };
+        }
+    }
+}
